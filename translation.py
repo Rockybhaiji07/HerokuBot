8ba2b02a90b9a6e9b6b1c571579a937c0ceb2ade
Rockybhaiji07 <86160003+Rockybhaiji07@users.noreply.github.com>
2025-01-21 18:59:55 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,11 @@
 class Translation(object):
 
       # Start text
-      START = "Hi there! I am a channel auto forward bot. I can forward new messages & Media from one channel to another.\n\n• Source code:<b/> <b><a href='https://github.com/0AIB/Auto-Forward-Bot'>Auto Forward Bot</a><b/>"             
+      START = "Hi there! I am a cricket Line bot. I can give live  new messages & Media r.\n\n• Update Channel:<b/> <b><a href='https://t.me/herokutech'>Cricket Live line</a><b/>"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> @lx0980AI
-<b>🌐Source Code:</b> https://github.com/0AIB/Auto-Forward-Bot"""
+<b>👨‍💻 Developer:</b> @OwnerRocky
+<b>🌐Contact For Line:</b>https://t.me/OwnerRocky"""
